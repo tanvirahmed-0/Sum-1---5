60460b34dd312ef3a1c597610e7e9d81e41f4b04
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-12-09 16:17:28 +0600

    Add files via upload

---
+++ Sum 1 - 5/index.js
@@ -0,0 +1,22 @@
+// for(var i = 10; i >= 1; i--){
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 100; i++) {
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// 
+
+
+
+
+
+let sum = 0;
+for (let i = 1; i <= 23; i++) {
+    console.log(sum)
+    sum +=i;
+}
+
+document.write(sum)
+
